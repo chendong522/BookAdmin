5944b2592e15c6082c0b431b481481c58ea41f4e
tianbo <1466160757@qq.com>
2018-10-15 22:29:58 +0800

    图书借阅数量-1存在问题

--- routes/index.js
+++ routes/index.js
@@ -10,57 +10,70 @@ var querystring = require("querystring");
 /* GET home page. */
 //默认根目录加载登录界面
 router.get('/', function(req, res, next) {
-  	req.session.username = undefined;
-  	res.render('index',{});
+	req.session.username = undefined;
+	res.render('index', {});
 });
 
 //注册页面渲染
-router.get("/register",(req,res)=>{
-	res.render("register",{});
+router.get("/register", (req, res) => {
+	res.render("register", {});
 });
 
 //找回密码页面渲染
-router.get("/rePass",(req,res)=>{
-	res.render("rePass",{});
+router.get("/rePass", (req, res) => {
+	res.render("rePass", {});
 })
 
 //系统界面渲染
-router.get("/home",(req,res)=>{
-	res.render("home",{username:req.session.username,infoUrl:"home_index"});
+router.get("/home", (req, res) => {
+	res.render("home", {
+		username: req.session.username,
+		infoUrl: "home_index"
+	});
 })
 
 //图书列表页面渲染
-router.get("/bookdetail",(req,res)=>{
+router.get("/bookdetail", (req, res) => {
 	//默认加载数据库前面的图书信息
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			//数据总条数
 			var count = 0;
 			//总页数
-			var allPages = 0;			
+			var allPages = 0;
 			//每页条数
 			var size = 5;
 			//页码
 			var page = req.query.page;
-			page = page?page:1;
-			
+			page = page ? page : 1;
+
 			//异步加载流程
 			async.series([
-				(callback)=>{
-					coll.find({}).sort({_id:-1}).toArray((err,data)=>{
-						count = data.length;//总条数
-						allPages = Math.ceil(count/size);//总页数
-						page = page<1?1:page>allPages?allPages:page;
+				(callback) => {
+					coll.find({}).sort({
+						_id: -1
+					}).toArray((err, data) => {
+						count = data.length; //总条数
+						allPages = Math.ceil(count / size); //总页数
+						page = page < 1 ? 1 : page > allPages ? allPages : page;
 					})
-					callback(null,"");
+					callback(null, "");
 				},
-				(callback)=>{
-					coll.find({}).sort({_id:-1}).limit(size).skip((page-1)*size).toArray((err,data)=>{
-						callback(null,data);
+				(callback) => {
+					coll.find({}).sort({
+						_id: -1
+					}).limit(size).skip((page - 1) * size).toArray((err, data) => {
+						callback(null, data);
 					})
 				}
-			],(err,data)=>{
-				res.render("home",{username:req.session.username,infoUrl:"bookdetail",info:data[1],page:page,allPages:allPages});
+			], (err, data) => {
+				res.render("home", {
+					username: req.session.username,
+					infoUrl: "bookdetail",
+					info: data[1],
+					page: page,
+					allPages: allPages
+				});
 				database.close();
 			})
 		})
@@ -68,123 +81,243 @@ router.get("/bookdetail",(req,res)=>{
 })
 
 //新书上架页面
-router.get("/newbook",(req,res)=>{
-	res.render("home",{username:req.session.username,infoUrl:"newbook"});
+router.get("/newbook", (req, res) => {
+	res.render("home", {
+		username: req.session.username,
+		infoUrl: "newbook"
+	});
 })
-
 
 //图书查询成功显示结果
-router.get("/findbook1",(req,res)=>{
+router.get("/findbook1", (req, res) => {
 	//console.log(req.query.bid);
-	var id = req.query.bid;  //获取查询图书的ID
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+	var id = req.query.bid; //获取查询图书的ID
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			//数据总条数
 			var count = 0;
 			//总页数
-			var allPages = 0;			
+			var allPages = 0;
 			//每页条数
 			var size = 5;
 			//页码
 			var page = req.query.page;
-			page = page?page:1;
-			
+			page = page ? page : 1;
+
 			//异步加载流程
 			async.series([
-				(callback)=>{
-					coll.find({newbid:id}).sort({_id:-1}).toArray((err,data)=>{
-						count = data.length;//总条数
-						allPages = Math.ceil(count/size);//总页数
-						page = page<1?1:page>allPages?allPages:page;
+				(callback) => {
+					coll.find({
+						newbid: id
+					}).sort({
+						_id: -1
+					}).toArray((err, data) => {
+						count = data.length; //总条数
+						allPages = Math.ceil(count / size); //总页数
+						page = page < 1 ? 1 : page > allPages ? allPages : page;
 					})
-					callback(null,"");
+					callback(null, "");
 				},
-				(callback)=>{
-					coll.find({newbid:id}).sort({_id:-1}).limit(size).skip((page-1)*size).toArray((err,data)=>{
-						callback(null,data);
+				(callback) => {
+					coll.find({
+						newbid: id
+					}).sort({
+						_id: -1
+					}).limit(size).skip((page - 1) * size).toArray((err, data) => {
+						callback(null, data);
 					})
 				}
-			],(err,data)=>{
-				res.render("home",{username:req.session.username,infoUrl:"findbook",info:data[1],page:page,allPages:allPages});
+			], (err, data) => {
+				res.render("home", {
+					username: req.session.username,
+					infoUrl: "findbook",
+					info: data[1],
+					page: page,
+					allPages: allPages
+				});
 				database.close();
 			})
 		})
 	})
 })
 
-router.get("/findbook2",(req,res)=>{
+router.get("/findbook2", (req, res) => {
 	var bname = querystring.unescape(req.query.bname);
-	var reg = new RegExp("^"+bname);  //构造函数创建包含变量的正则
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+	var reg = new RegExp("^" + bname); //构造函数创建包含变量的正则
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			//数据总条数
 			var count = 0;
 			//总页数
-			var allPages = 0;			
+			var allPages = 0;
 			//每页条数
 			var size = 5;
 			//页码
 			var page = req.query.page;
-			page = page?page:1;
-			
+			page = page ? page : 1;
+
 			//异步加载流程
 			async.series([
-				(callback)=>{
-					coll.find({newbname:reg}).sort({_id:-1}).toArray((err,data)=>{
-						count = data.length;//总条数
-						allPages = Math.ceil(count/size);//总页数
-						page = page<1?1:page>allPages?allPages:page;
+				(callback) => {
+					coll.find({
+						newbname: reg
+					}).sort({
+						_id: -1
+					}).toArray((err, data) => {
+						count = data.length; //总条数
+						allPages = Math.ceil(count / size); //总页数
+						page = page < 1 ? 1 : page > allPages ? allPages : page;
 					})
-					callback(null,"");
+					callback(null, "");
 				},
-				(callback)=>{
-					coll.find({newbname:reg}).sort({_id:-1}).limit(size).skip((page-1)*size).toArray((err,data)=>{
-						callback(null,data);
+				(callback) => {
+					coll.find({
+						newbname: reg
+					}).sort({
+						_id: -1
+					}).limit(size).skip((page - 1) * size).toArray((err, data) => {
+						callback(null, data);
 					})
 				}
-			],(err,data)=>{
-				res.render("home",{username:req.session.username,infoUrl:"findbook",info:data[1],page:page,allPages:allPages});
+			], (err, data) => {
+				res.render("home", {
+					username: req.session.username,
+					infoUrl: "findbook",
+					info: data[1],
+					page: page,
+					allPages: allPages
+				});
 				database.close();
 			})
 		})
 	})
 })
 
-router.get("/findbook3",(req,res)=>{
+router.get("/findbook3", (req, res) => {
 	var bkind = querystring.unescape(req.query.bkind);
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			//数据总条数
 			var count = 0;
 			//总页数
-			var allPages = 0;			
+			var allPages = 0;
 			//每页条数
 			var size = 5;
 			//页码
 			var page = req.query.page;
-			page = page?page:1;
-			
+			page = page ? page : 1;
+
 			//异步加载流程
 			async.series([
-				(callback)=>{
-					coll.find({newbkind:bkind}).sort({_id:-1}).toArray((err,data)=>{
-						count = data.length;//总条数
-						allPages = Math.ceil(count/size);//总页数
-						page = page<1?1:page>allPages?allPages:page;
+				(callback) => {
+					coll.find({
+						newbkind: bkind
+					}).sort({
+						_id: -1
+					}).toArray((err, data) => {
+						count = data.length; //总条数
+						allPages = Math.ceil(count / size); //总页数
+						page = page < 1 ? 1 : page > allPages ? allPages : page;
 					})
-					callback(null,"");
+					callback(null, "");
 				},
-				(callback)=>{
-					coll.find({newbkind:bkind}).sort({_id:-1}).limit(size).skip((page-1)*size).toArray((err,data)=>{
-						callback(null,data);
+				(callback) => {
+					coll.find({
+						newbkind: bkind
+					}).sort({
+						_id: -1
+					}).limit(size).skip((page - 1) * size).toArray((err, data) => {
+						callback(null, data);
 					})
 				}
-			],(err,data)=>{
-				res.render("home",{username:req.session.username,infoUrl:"findbook",info:data[1],page:page,allPages:allPages});
+			], (err, data) => {
+				res.render("home", {
+					username: req.session.username,
+					infoUrl: "findbook",
+					info: data[1],
+					page: page,
+					allPages: allPages
+				});
 				database.close();
 			})
 		})
 	})
 })
+
+
+//学生个人信息添加页面渲染
+router.get("/stuinfo_add",(req,res)=>{
+	//先判断该学生信息是否已经存在
+	mongodb.connect(db_str,(err,database)=>{
+		database.collection("stuinfo",(err,coll)=>{
+			coll.find({stuadname:req.session.username}).toArray((err,data)=>{
+				if(data.length>0){
+					res.render("home",{username:req.session.username,infoUrl:"stuinfo_add",data:data[0]});
+				}else{
+					res.render("home",{username:req.session.username,infoUrl:"stuinfo_add",data:""});
+				}
+				database.close();
+			})
+		})
+	})
+})
+
+//学生个人信息查看修改页面渲染
+router.get("/stuinfo_up",(req,res)=>{
+	res.render("home",{username:req.session.username,infoUrl:"stuinfo_up"});
+})
+
+
+//图书借阅界面渲染
+router.get("/stubor",(req,res)=>{
+	mongodb.connect(db_str,(err,database)=>{
+		database.collection("book",(err,coll)=>{
+			coll.find({}).toArray((err,data)=>{
+				//数据总条数
+				var count = 0;
+				//总页数
+				var allPages = 0;
+				//每页条数
+				var size = 7;
+				//页码
+				var page = req.query.page;
+				page = page ? page : 1;
+	
+				//异步加载流程
+				async.series([
+					(callback) => {
+						coll.find({}).sort({
+							_id: -1
+						}).toArray((err, data) => {
+							count = data.length; //总条数
+							allPages = Math.ceil(count / size); //总页数
+							page = page < 1 ? 1 : page > allPages ? allPages : page;
+						})
+						callback(null, "");
+					},
+					(callback) => {
+						coll.find({}).sort({
+							_id: -1
+						}).limit(size).skip((page - 1) * size).toArray((err, data) => {
+							callback(null, data);
+						})
+					}
+				], (err, data) => {
+					res.render("home", {
+						username: req.session.username,
+						infoUrl: "stubor",
+						info: data[1],
+						page: page,
+						allPages: allPages
+					});
+					database.close();
+				})
+			})
+		})
+	})
+})
+
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,34 @@ var mongodb = require("mongodb").MongoClient;
 var db_str = "mongodb://localhost:27017/Pro";
 var ObjectId = require("mongodb").ObjectId;
 var querystring = require("querystring");
+var async = require("async");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
 });
 
 //注册数据操作
-router.post("/register",(req,res)=>{
+router.post("/register", (req, res) => {
 	//连接数据库
-	mongodb.connect(db_str,(err,database)=>{
+	mongodb.connect(db_str, (err, database) => {
 		//连接集合
-		database.collection("user",(err,coll)=>{
-			coll.find({username:req.body["username"]}).toArray((err,data)=>{
-				if(data.length>0){
-					res.send(JSON.stringify({code:0,msg:"注册失败"}));
+		database.collection("user", (err, coll) => {
+			coll.find({
+				username: req.body["username"]
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "注册失败"
+					}));
 					database.close();
-				}else{
-					coll.save(req.body,()=>{
-						res.send(JSON.stringify({code:1,msg:"注册成功"}));
+				} else {
+					coll.save(req.body, () => {
+						res.send(JSON.stringify({
+							code: 1,
+							msg: "注册成功"
+						}));
 						database.close();
 					});
 				}
@@ -33,16 +42,25 @@ router.post("/register",(req,res)=>{
 })
 
 //登陆数据处理
-router.post("/login",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("user",(err,coll)=>{
-			coll.find({username:req.body["username"],password:req.body["password"]}).toArray((err,data)=>{
-				if(data.length>0){
+router.post("/login", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("user", (err, coll) => {
+			coll.find({
+				username: req.body["username"],
+				password: req.body["password"]
+			}).toArray((err, data) => {
+				if(data.length > 0) {
 					//用户数据存入session
 					req.session.username = data[0].username;
-					res.send(JSON.stringify({code:1,msg:"登陆成功"}));
-				}else{
-					res.send(JSON.stringify({code:0,msg:"登陆失败"}));
+					res.send(JSON.stringify({
+						code: 1,
+						msg: "登陆成功"
+					}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "登陆失败"
+					}));
 				}
 				database.close();
 			})
@@ -51,49 +69,74 @@ router.post("/login",(req,res)=>{
 })
 
 //找回密码数据操作
-router.post("/rePass",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("user",(err,coll)=>{
-			coll.find({username:req.body.username,tel:req.body.tel}).toArray((err,data)=>{
-				if(data.length>0){	
-					coll.update({username:req.body.username,tel:req.body.tel},{$set:{password:req.body.password}},()=>{
+router.post("/rePass", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("user", (err, coll) => {
+			coll.find({
+				username: req.body.username,
+				tel: req.body.tel
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					coll.update({
+						username: req.body.username,
+						tel: req.body.tel
+					}, {
+						$set: {
+							password: req.body.password
+						}
+					}, () => {
 						//密码重置成功直接跳转进入系统
 						req.session.username = data[0].username;
-						res.send(JSON.stringify({code:1,msg:"密码重置成功"}));
+						res.send(JSON.stringify({
+							code: 1,
+							msg: "密码重置成功"
+						}));
 					})
-				}else{
-					res.send(JSON.stringify({code:0,msg:"密码重置失败"}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "密码重置失败"
+					}));
 				}
 				database.close();
 			})
 		})
 	})
 })
-
 
 //新书上架
-router.post("/newbook",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
-			coll.save(req.body,()=>{
-				res.send(JSON.stringify({code:1,msg:"插入成功！"}));
+router.post("/newbook", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
+			coll.save(req.body, () => {
+				res.send(JSON.stringify({
+					code: 1,
+					msg: "插入成功！"
+				}));
 				database.close();
 			})
 		})
 	})
 })
 
-
 //编号查询图书
-router.get("/findbook1",(req,res)=>{
+router.get("/findbook1", (req, res) => {
 	var bid = req.query.newbid;
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
-			coll.find({newbid:bid}).toArray((err,data)=>{
-				if(data.length>0){
-					res.send(JSON.stringify({code:1,msg:"查询成功"}));
-				}else{
-					res.send(JSON.stringify({code:0,msg:"未搜索到相应数据"}));
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
+			coll.find({
+				newbid: bid
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send(JSON.stringify({
+						code: 1,
+						msg: "查询成功"
+					}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "未搜索到相应数据"
+					}));
 				}
 				database.close();
 			})
@@ -101,18 +144,25 @@ router.get("/findbook1",(req,res)=>{
 	})
 })
 
-
 //书名模糊查找
-router.get("/findbook2",(req,res)=>{
-	var bname = querystring.unescape(req.query.newbname);  //获取请求路径上名称信息
-	var reg = new RegExp("^"+bname);  //构造函数创建包含变量的正则
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
-			coll.find({newbname:reg}).toArray((err,data)=>{
-				if(data.length>0){
-					res.send(JSON.stringify({code:1,msg:"查询成功"}));
-				}else{
-					res.send(JSON.stringify({code:0,msg:"查询失败"}));
+router.get("/findbook2", (req, res) => {
+	var bname = querystring.unescape(req.query.newbname); //获取请求路径上名称信息
+	var reg = new RegExp("^" + bname); //构造函数创建包含变量的正则
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
+			coll.find({
+				newbname: reg
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send(JSON.stringify({
+						code: 1,
+						msg: "查询成功"
+					}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "查询失败"
+					}));
 				}
 				database.close();
 			})
@@ -121,15 +171,23 @@ router.get("/findbook2",(req,res)=>{
 })
 
 //图书类别查找
-router.get("/findbook3",(req,res)=>{
-	var bkind = querystring.unescape(req.query.newbkind);  //获取请求路径上类别信息
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
-			coll.find({newbkind:bkind}).toArray((err,data)=>{
-				if(data.length>0){
-					res.send(JSON.stringify({code:1,msg:"查询成功"}));
-				}else{
-					res.send(JSON.stringify({code:0,msg:"查询失败"}));
+router.get("/findbook3", (req, res) => {
+	var bkind = querystring.unescape(req.query.newbkind); //获取请求路径上类别信息
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
+			coll.find({
+				newbkind: bkind
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send(JSON.stringify({
+						code: 1,
+						msg: "查询成功"
+					}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "查询失败"
+					}));
 				}
 				database.close();
 			})
@@ -137,18 +195,18 @@ router.get("/findbook3",(req,res)=>{
 	})
 })
 
-
-
-
-
-
 //删除图书操作
-router.post("/rmbook",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+router.post("/rmbook", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			var id = ObjectId(req.body.id);
-			coll.remove({_id:id},()=>{
-				res.send(JSON.stringify({code:1,msg:"删除成功"}));
+			coll.remove({
+				_id: id
+			}, () => {
+				res.send(JSON.stringify({
+					code: 1,
+					msg: "删除成功"
+				}));
 				database.close();
 			})
 		})
@@ -156,15 +214,24 @@ router.post("/rmbook",(req,res)=>{
 })
 
 //修改图书信息操作
-router.post("/upbook",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+router.post("/upbook", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			var id = ObjectId(req.body._id);
-			coll.find({_id:id}).toArray((err,data)=>{
-				if(data.length>0){
-					res.send(JSON.stringify({code:1,msg:"信息查询成功",data:data[0]}));
-				}else{
-					res.send(JSON.stringify({code:0,msg:"无相应信息"}))
+			coll.find({
+				_id: id
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send(JSON.stringify({
+						code: 1,
+						msg: "信息查询成功",
+						data: data[0]
+					}));
+				} else {
+					res.send(JSON.stringify({
+						code: 0,
+						msg: "无相应信息"
+					}))
 				}
 				database.close();
 			})
@@ -172,15 +239,145 @@ router.post("/upbook",(req,res)=>{
 	})
 })
 
-router.post("/upbook2",(req,res)=>{
-	mongodb.connect(db_str,(err,database)=>{
-		database.collection("book",(err,coll)=>{
+router.post("/upbook2", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("book", (err, coll) => {
 			var id = ObjectId(req.body._id);
-			coll.update({_id:id},{$set:{newbid:req.body.newbid,newbkind:req.body.newbkind,newbname:req.body.newbname,newbwriter:req.body.newbwriter,newbdate:req.body.newbdate,newbpress:req.body.newbpress,newbinum:req.body.newbinum,newbaddnum:req.body.newbaddnum,newbother:req.body.newbother}},()=>{
-				res.send(JSON.stringify({code:1,msg:"修改成功"}));
+			coll.update({
+				_id: id
+			}, {
+				$set: {
+					newbid: req.body.newbid,
+					newbkind: req.body.newbkind,
+					newbname: req.body.newbname,
+					newbwriter: req.body.newbwriter,
+					newbdate: req.body.newbdate,
+					newbpress: req.body.newbpress,
+					newbinum: req.body.newbinum,
+					newbaddnum: req.body.newbaddnum,
+					newbother: req.body.newbother
+				}
+			}, () => {
+				res.send(JSON.stringify({
+					code: 1,
+					msg: "修改成功"
+				}));
 				database.close();
 			})
 		})
+	})
+})
+
+//学生信息添加
+router.post("/stuinfo_add", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("stuinfo", (err, coll) => {
+			coll.save(req.body, () => {
+				res.send(JSON.stringify({
+					code: 1,
+					msg: "添加成功"
+				}));
+				database.close();
+			})
+		})
+	})
+})
+
+//获取当前学生信息    加载学生信息页面时直接发送ajax请求获取当前登录的用户的信息，如果直接用页面渲染，后期用户资料信息更改麻烦
+router.get("/stuinfo_up1", (req, res) => {
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("stuinfo", (err, coll) => {
+			coll.find({
+				stuadname: req.session.username
+			}).toArray((err, data) => {
+				if(data.length > 0) {
+					res.send({
+						code: 1,
+						msg: "信息获取成功",
+						data: data[0]
+					});
+				} else {
+					res.send({
+						code: 0,
+						msg: "无相应信息"
+					});
+				}
+				database.close();
+			})
+		})
+	})
+})
+
+//学生信息修改
+router.post("/stuinfo_up2", (req, res) => {
+	var stu_id = ObjectId(req.body.stu_id);
+	mongodb.connect(db_str, (err, database) => {
+		database.collection("stuinfo", (err, coll) => {
+			coll.update({
+				_id: stu_id
+			}, {
+				$set: {
+					stunum: req.body.stunum,
+					studept: req.body.studept,
+					stuname: req.body.stuname,
+					stusex: req.body.stusex,
+					studate: req.body.studate,
+					stutel: req.body.stutel,
+					stuemail: req.body.stuemail,
+					stuadname: req.body.stuadname,
+					stuother: req.body.stuother
+				}
+			}, () => {
+				res.send(JSON.stringify({
+					code: 1,
+					msg: "修改成功"
+				}));
+				database.close();
+			})
+		})
+	})
+})
+
+//图书借阅操作
+router.post("/bookbor", (req, res) => {
+	//借阅书的_id值
+	var b_id = ObjectId(req.body.b_id);
+	console.log(b_id);
+	//连接数据库   1.图书表中指定书数量-1   2.图书借阅表中插入新数据
+	async.series([
+		(callback)=>{
+			mongodb.connect(db_str,(err,database)=>{
+				database.collection("book",(err,coll)=>{
+					//指定书数量-1
+					coll.update({_id:b_id},{$inc:{newbaddnum:-1}},()=>{
+						//获取书名
+						coll.find({_id:b_id}).toArray((err,data)=>{
+							callback(null,data[0].newbname);
+						})	
+					})									
+				})
+			})
+		},(callback)=>{
+			mongodb.connect(db_str,(err,database)=>{
+				database.collection("stuinfo",(err,coll)=>{
+					//获取登录用户姓名
+					coll.find({stuadname:req.session.username}).toArray((err,data)=>{
+						callback(null,data[0].stuname);
+					})
+				})
+			})
+		}
+	],(err,data)=>{
+		console.log(data);
+		//data:[书名,用户名]
+		mongodb.connect(db_str,(err,database)=>{
+			database.collection("bookbor",(err,coll)=>{
+				coll.save({stuname:data[1],book:data[0]},()=>{
+					res.send(JSON.stringify({code:1}));
+				});
+			})
+		})
+		
 	})
 })
 
